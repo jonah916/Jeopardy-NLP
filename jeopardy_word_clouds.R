c5d2151629e15406242c3a6248e26570b56840cd
Jonah Wiener-Brodkey <58953110+jonah916@users.noreply.github.com>
2022-09-10 23:42:50 -0400

    adding comments

--- jeopardy_word_clouds.R
+++ jeopardy_word_clouds.R
@@ -1,74 +1,62 @@
+# loading libraries
+
 library(readr)
 library(dplyr)
 library(tm)
 library(wordcloud)
 library(RColorBrewer)
 
+# importing data
+
 jeopardy <- read_delim("~/Jeopardy/master_season1-35.tsv/master_season1-35.tsv", 
                                 "\t", escape_double = FALSE, trim_ws = TRUE)
 
+# filter to clues used in past college tournaments
 college <- jeopardy[grepl("College", jeopardy$notes),]
 
-# daily doubles
-
-daily_double_j <- college %>% 
-  filter(daily_double == "yes",
-         air_date > "2001-11-26",
-         round == 1,
-         value %in% c(seq(200, 1000, by = 200))) %>% 
-  group_by(value) %>% 
-  summarise(count = n())
-daily_double_j <- daily_double_j %>% mutate(prop = count/sum(count))
-daily_double_dj <- college %>% 
-  filter(daily_double == "yes",
-         air_date > "2001-11-26",
-         round == 1,
-         value %in% c(seq(400, 2000, by = 400))) %>% 
-  group_by(value) %>% 
-  summarise(count = n())
-daily_double_dj <- daily_double_dj %>% mutate(prop = count/sum(count))
-
-##### 
-
-college %>% filter(round != 3) %>% group_by(notes, category) %>% summarise(count = n()) %>% filter(category == "SHAKESPEARE")
-
-# most common questions
-
-
-
-# most common categories
+# finding the most common categories in college tournaments
 
 common_categories <- college %>%
   group_by(category) %>%
   summarise(count = n()) %>%
   arrange(desc(count))
 
-# POETRY
+# getting the data for a given category
 
-categories_to_use <- common_categories$category[grepl("MOVIE|FILM|CINEMA", common_categories$category)]
+regex <- "" # EDIT THIS WITH REGEX FOR DESIRED CATEGORY/CATEGORIES
+categories_to_use <- common_categories$category[grepl(regex, common_categories$category)]
+# filter down to just the clues for the desired category/categories
 clues_to_use <- jeopardy[jeopardy$category %in% categories_to_use,]
+# get rid of parentheses
 clues_to_use$question <- gsub("[()]", "", clues_to_use$question)
+# get rid of "a," "an," and "the" (except for categories like Books that deal with titles of works)
 clues_to_use$question <- trimws(tolower(removeWords(clues_to_use$question, c("a","an", "the")))) # comment out when dealing with titles
+# trimming whitespace
 clues_to_use$question <- trimws(removePunctuation(tolower(clues_to_use$question)))
+
+# getting the most common questions for the given category and only keep those appearing at least twice
+
 questions_to_use <- clues_to_use %>%
   group_by(question) %>%
   summarise(count = n()) %>%
   arrange(desc(count)) %>% 
   filter(count >= 2)
 
+# initializing dataframe to hold each pair of questions and file paths to word clouds
+
 anki_df <- data.frame(character(nrow(questions_to_use)), character(nrow(questions_to_use)), stringsAsFactors = FALSE)
 j <- 1
 
 # word clouds
 
-
+# for each question in the list of common questions for this category
 for (i in questions_to_use$question){
   
   ## create corpus
   text <- jeopardy$answer[which(jeopardy$question == i)]
   docs <- Corpus(VectorSource(text))
 
-  ## clean text
+  ## clean text and remove stop words
   docs <- docs %>%
     tm_map(removeNumbers) %>%
     tm_map(removePunctuation) %>%
@@ -76,18 +64,13 @@ for (i in questions_to_use$question){
   docs <- tm_map(docs, content_transformer(tolower))
   docs <- tm_map(docs, removeWords, stopwords("english"))
 
-  ## document term matrix
+  ## create document term matrix
   dtm <- TermDocumentMatrix(docs, control = list( wordLengths = c(0,Inf) ))
   matrix <- as.matrix(dtm)
   words <- sort(rowSums(matrix),decreasing=TRUE)
   df <- data.frame(word = names(words),freq=words)
-  #  filter(!(word %in% c("shakespeare", "shakespeares", "shakespearean", "city", "citys", "cities", "play", "plays", "character", "characters")))
-  #df <- df[-which(df$word %in% c("country", "state", "countrys", "continent", "called")),]
-  #df <- df[-which(df$word %in% c("poet", "wrote", "poem", "poets", "poems", "story", "stories")),]
-  #df <- df[-which(df$word %in% c("city", "citys", "country", "countries", "countrys", "cities", "capital", "capitals")),]
-  #df <- df[-which(df$word %in% c("shakespeare", "shakespeares", "shakespearean", "city", "citys", "cities")),]
 
-  ## create word cloud - save to jeopardy folder
+  ## create word cloud & save to jeopardy folder
   set.seed(1234) # for reproducibility
   png(filename = paste0("wordcloud_", removePunctuation(i), ".png"), width = 350, height = 350)
   wordcloud(words = df$word, freq = df$freq, min.freq = 0, max.words=200, random.order=FALSE, rot.per=0.2, colors=brewer.pal(8, "Dark2"), scale = c(3.5,0.6))
@@ -99,12 +82,14 @@ for (i in questions_to_use$question){
   wordcloud(words = df$word, freq = df$freq, min.freq = 2, max.words=200, random.order=FALSE, rot.per=0.2, colors=brewer.pal(8, "Dark2"), scale = c(3.5,0.6))
   dev.off()
   
-  ## save to csv
+  ## save to dataframe of questions and word cloud paths that will be used by Anki
   anki_df[j,] <- c(i, paste0("<img src='wordcloud_", removePunctuation(i), ".png' />")) 
   j <- j + 1
 }
 
-write.table(anki_df, "BIOLOGY.csv", col.names = FALSE, row.names = FALSE, sep = ",")
+##
+csv_name <- "" ## EDIT WITH NAME OF CURRENT CSV FOR ANKI
+write.table(anki_df, csv_name, col.names = FALSE, row.names = FALSE, sep = ",")
 
 # FUNCTION
 
@@ -120,19 +105,27 @@ get_wordclouds <- function(categ, min.frequency = 2, omit.words = NULL, anki.nam
  
   # categories and questions filtering
   categories_to_use <- common_categories$category[grepl(categ, common_categories$category)]
+  ## filter down to just the clues for the desired category/categories
   clues_to_use <- jeopardy[jeopardy$category %in% categories_to_use,]
+  ## get rid of parentheses
   clues_to_use$question <- gsub("[()]", "", clues_to_use$question)
   clues_to_use$question_cleaned <- clues_to_use$question
-  #clues_to_use$question_cleaned <- removeWords(clues_to_use$question, c("a","an","the")) # comment out when dealing with titles
+  ## get rid of "a," "an," and "the" (except for categories like Books that deal with titles of works)
+  clues_to_use$question_cleaned <- removeWords(clues_to_use$question, c("a","an","the")) # comment out when dealing with titles
+  ## getting ride of punctuation
   clues_to_use$question_cleaned <- trimws(removePunctuation(tolower(clues_to_use$question_cleaned)))
-  #clues_to_use$question_cleaned <- trimws(removePunctuation(tolower(clues_to_use$question)))
+  ## stripping whitespace
+  clues_to_use$question_cleaned <- trimws(removePunctuation(tolower(clues_to_use$question)))
+  
+  # getting the most common questions for the given category and only keep those appearing at least MIN FREQUENCY times
+  
   questions_to_use <- clues_to_use %>%
     group_by(question_cleaned) %>%
     summarise(count = n()) %>%
     arrange(desc(count)) %>% 
     filter(count >= min.frequency)
   
-  # initialize data frame for Anki
+  # initialize data frame for Anki, which will have question topics and file paths for their respective word clouds
   anki_df <- data.frame(character(nrow(questions_to_use)), character(nrow(questions_to_use)), stringsAsFactors = FALSE)
   j <- 1
   
@@ -157,9 +150,10 @@ get_wordclouds <- function(categ, min.frequency = 2, omit.words = NULL, anki.nam
     matrix <- as.matrix(dtm)
     words <- sort(rowSums(matrix),decreasing=TRUE)
     df <- data.frame(word = names(words),freq=words)
+    ### remove unnecessary words specified in the function call
     if(!is.null(tolower(omit.words))){df <- filter(df, !(word %in% tolower(omit.words)))}
     
-    ## create word cloud - save to jeopardy folder
+    ## create word cloud & save to jeopardy folder
     set.seed(1234) # for reproducibility
     png(filename = paste0("wordcloud_", removePunctuation(i), ".png"), width = 350, height = 350)
     wordcloud(words = df$word, freq = df$freq, min.freq = 3, max.words=200, random.order=FALSE, rot.per=0.2, colors=brewer.pal(8, "Dark2"), scale = c(3.5,0.6))
@@ -176,48 +170,11 @@ get_wordclouds <- function(categ, min.frequency = 2, omit.words = NULL, anki.nam
     j <- j + 1
   }
   
-  # save csv for anki
+  # save csv to be used by anki
   write.table(anki_df, paste0(anki.name,".csv"), col.names = FALSE, row.names = FALSE, sep = ",")
-  
-  
+ 
 }
 
-# BIOLOGY
+# example: BIOLOGY
 
 get_wordclouds(categ = "BIOLOGY", min.frequency = 3, anki.name = "BIOLOGY")
-
-# NONFICTION
-
-get_wordclouds(categ = "NONFICTION", min.frequency = 2, anki.name = "NONFICTION")
-
-# POETS AND POETRY
-
-get_wordclouds(categ = "POET", min.frequency = 3, omit.words = c("POET","POETS","WROTE","WRITTEN","POETRY"), anki.name = "POETS & POETRY")
-
-# WORD ORIGINS
-
-get_wordclouds(categ = "WORD.+ORIGINS", min.frequency = 2, anki.name = "WORD ORIGINS")
-
-# COLLEGES & UNIVERSITIES
-
-get_wordclouds(categ = "COLLEG", min.frequency = 2, anki.name = "COLLEGES & UNIVERSITIES")
-
-# US HISTORY
-
-get_wordclouds(categ = "(U.S.|AMERICAN) HISTOR", min.frequency = 3, omit.words = c("STATE", "STATES", "CITY", "CITIES"), anki.name = "US HISTORY")
-
-# CLIFF'S NOTES
-
-get_wordclouds(categ = "CLIFF", min.frequency = 1, anki.name = "CLIFFS NOTES")
-
-# THE BIBLE
-
-get_wordclouds(categ = "BIBL", min.frequency = 3, anki.name = "THE BIBLE")
-
-# ISLANDS
-
-get_wordclouds(categ = "ISLAND", min.frequency = 2, omit.words = c("ISLAND", "ISLANDS", "COUNTRY", "COUNTRIES", "NATION", "NATIONs"), anki.name = "ISLANDS")
-
-# MOVIES
-
-get_wordclouds(categ = "MOVIE|FILM|CINEMA", min.frequency = 2, omit.words = c("MOVIE", "MOVIES", "FILM", "FILMS", "CINEMATIC", "CINEMA", "ACTOR", "ACTORS", "ACTRESS"), anki.name = "MOVIES")
